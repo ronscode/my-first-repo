9e6598ab7f85920719ce7453dc85b39315700854
Ron Cagle <ron@atlantashirt.com>
2018-09-20 21:16:47 -0400

    Test

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function marinaraSauce(cookName) {
     console.log("Cover the pot, bring to a boil, reuce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
     console.log("Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
     console.log("Hello");
+    console.log("Hello 2");
     function enchillada () {
 
     }
